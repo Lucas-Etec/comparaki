0bd77d6d55f013a65e7ad8585f72aa8ba789323b
Matheus Mattos <matheus1336.mattos@gmail.com>
2023-10-29 16:01:55 -0300

    create and edit - header.php

---
+++ header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> ComparaKi</title>
+    <link rel="stylesheet" rel="style.css">
+</head>
+
+<body>
+    <header>
+        <nav class="navbar navbar-light bg-light">
+            <form class="form-inline">
+                <input class="form-control mr-sm-2" type="search" placeholder="Pesquisar" aria-label="Pesquisar">
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Pesquisar</button>
+            </form>
+            <div>
+                <a href="?pagina=home"> <img src="logo.png" title="logo" alt="logo ComparaKi"> </a>
+                pesquisar
+                <i class="bi bi-pencil-square"></i>
+            </div>
+        </nav>
+    </header>
+</body>
