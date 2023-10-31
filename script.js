d103baad47602c48e794af57184c5fb750f0560d
Lucas-Etec <127600893+Lucas-Etec@users.noreply.github.com>
2023-10-30 22:44:36 -0300

    criado função trocar pagina

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
-fetch('pages/categorias.html')
-    .then(response => response.text())
-    .then(data => {
-        const container = document.getElementById('conteudo');
-        const containerContent = data;
-        container.innerHTML += containerContent;
-    });
+function carregarPagina(url) {
+    fetch(url)
+        .then(response => response.text())
+        .then(data => {
+            const container = document.getElementById('conteudo');
+            const containerContent = data;
+            container.innerHTML += containerContent;
+        });
+}
+
+carregarPagina('pages/categorias.html')
