c363c6ee9f6178f345e7e823aac5682183322068
lucasregisdemoraes <lucasgrm03@gmail.com>
2023-11-02 11:56:39 -0300

    criado função trocar página para comparação

--- js/produtos.js
+++ js/produtos.js
@@ -1,21 +1,31 @@
 const listElement = document.querySelector("#produtos ul")
+const produtosElement = document.querySelector("#produtos")
+const comparacaoElement = document.querySelector("#comparacao")
 
 export function mostrarProdutos(produtos) {
     listElement.innerHTML = ""
 
     produtos.forEach(produto => {
-        listElement.innerHTML += `
-            <li>
-                <img class="produtos" src="${produto.linkImagem}">
-                <div>
-                    <h2>${produto.nome} | ${produto.quantidade}</h2>
-                    <p class="cinza">${produto.marca}</p>
-                    <p> Melhor Preço:${produto.estabelecimento}<p>
-                            <h3> R$: ${produto.preco} </h3>
-                </div>
-                <button class="botao-produtos"> Ver comparação </button>
-                <p class="cinza comparacao"> Comparar entre 3 lojas </p>
-            </li>
+        const li = document.createElement("li")
+        li.innerHTML = `
+            <img class="produtos" src="${produto.linkImagem}">
+            <div>
+                <h2>${produto.nome} | ${produto.quantidade}</h2>
+                <pclass="cinza">${produto.marca}</p>
+                <p>Melhor Preço:${produto.estabelecimento}<p>
+                        <h3>R$: ${produto.preco}</h3>
+            </div>
+            <button class="botao-produtos">Ver comparação</button>
+            <p class="cinza comparacao">Comparar entre 3 lojas</p>
         `
+        li.querySelector("button").addEventListener("click", () => trocarPagina(produtos.indexOf(produto)))
+
+        listElement.appendChild(li)
     })
+}
+
+function trocarPagina(indexProduto) {
+    produtosElement.classList.add("desativo")
+    comparacaoElement.classList.remove("desativo")
+    console.log(indexProduto)
 }
