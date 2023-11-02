c50db97450f501cef22a6aec9281c9da56fb9345
Lucas-Etec <127600893+Lucas-Etec@users.noreply.github.com>
2023-11-01 21:47:57 -0300

    criado função trocar pagina

--- js/categorias.js
+++ js/categorias.js
@@ -1,18 +1,31 @@
 import categorias from "../dados/categorias.js"
+import produtos from "../dados/produtos.js"
+import { mostrarProdutos } from "./produtos.js"
 
 const listElement = document.querySelector("#categorias ul")
+const categoriasElement = document.querySelector("#categorias")
+const produtosElement = document.querySelector("#produtos")
 
 function mostrarCategorias(categorias) {
     listElement.innerHTML = ""
 
     categorias.forEach(categoria => {
-        listElement.innerHTML += `
-            <li>
-                ${categoria.tagIcone}
-                <span>${categoria.nome}</span>
-            </li>
+        const li = document.createElement("li")
+
+        li.innerHTML = `
+            ${categoria.tagIcone}
+            <span>${categoria.nome}</span>
         `
+        li.addEventListener("click", () => trocarPagina(categoria.nome))
+        listElement.appendChild(li)
     })
+}
+
+function trocarPagina(categoria) {
+    categoriasElement.classList.add("hide")
+    produtosElement.classList.remove("hide")
+
+    mostrarProdutos(produtos.filter(produto => produto.categoria === categoria.toLowerCase()))
 }
 
 mostrarCategorias(categorias)
