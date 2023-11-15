d71ebf7f518c84ec5347c8a7a8cb029e132ba547
lucasregisdemoraes <lucasgrm03@gmail.com>
2023-11-15 13:02:28 -0300

    terminado design da lista

--- js/lista.js
+++ js/lista.js
@@ -27,16 +27,16 @@ const funcoes = {
             const produto = produtos[index]
             const li = document.createElement("li")
             li.innerHTML = `
-                <li>
-                    <img src="${produto.linkImagem}" alt="${produto.nome}">
-                    <div>
-                        <h2>${produto.nome} | ${produto.quantidade}</h2>
-                        <p class="cinza">${produto.marca}</p>
-                        <p>${produto.estabelecimento}<p>
-                        <h3>R$: ${produto.preco}</h3>
-                    </div>
-                    <button type="button" class="botao-produtos">Remover</button>
-                </li>
+                <img src="${produto.linkImagem}" alt="${produto.nome}">
+                <div class="informacoes">
+                    <h2>${produto.nome} | ${produto.quantidade}</h2>
+                    <p class="cinza">${produto.marca}</p>
+                    <p>${produto.estabelecimento}<p>
+                    <h3>R$: ${produto.preco}</h3>
+                </div>
+                <button type="button">
+                    <i class="bi bi-trash"></i>
+                </button>
             `
             listaUl.appendChild(li)
         });
